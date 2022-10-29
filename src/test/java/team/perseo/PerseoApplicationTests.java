7d1ed21b03bc085ff48a428a3c1e977f228d9c61
Sergio Muñoz <sergiolopera.94@gmail.com>
2022-10-29 11:08:19 -0500

    Inicio del proyecto

---
+++ src/test/java/team/perseo/PerseoApplicationTests.java
@@ -0,0 +1,13 @@
+package team.perseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
