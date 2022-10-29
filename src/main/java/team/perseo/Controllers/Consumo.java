e822e45400fb29b6f9211f86bf48007c77782423
Sergio Muñoz <sergiolopera.94@gmail.com>
2022-10-29 12:25:18 -0500

    Agregada la plantilla

--- src/main/java/team/perseo/Controllers/Consumo.java
+++ src/main/java/team/perseo/Controllers/Consumo.java
@@ -1,5 +1,24 @@
 package team.perseo.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@RestController
 public class Consumo {
+
+    @RequestMapping("/prueba")
+    public String prueba(){
+        return "prueba";
+    }
+
+    @GetMapping(value = "getPrueba")
+    private String getPrueba(){
+        String uri = "http://localhost:8080/prueba";
+        RestTemplate prueba = new RestTemplate();
+        String result = prueba.getForObject(uri, String.class);
+        return  result;
+
+    }
 }
